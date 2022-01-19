d1e6d92ade32b0149383a3c94354ec18c7a1ec15
Mohammad Jahromi <mm.jahromi@gmail.com>
2022-01-19 10:55:37 -0700

    feat: add divide function

--- main.py
+++ main.py
@@ -3,16 +3,8 @@ def multiply(num1, num2, num3):
 
 
 
-def add(num1, num2):
-    """Adds two numbers together
-
-    Args:
-        num1 ([T]): First number
-        num2 ([T]): Second number
-
-    Returns:
-        [T]: Result of num1 * num2
-    """
-    return num1 + num2
+def divide(num1,num2):
+    "add two numbers"
+    return num1 / num2
 
 
