524b0157bedfdbebf7eae84dc399c32589eb25f0
PoStudent <matthew.vanderwey@ucalgary.ca>
2022-01-19 10:39:18 -0700

    feat: added docstring to add function

--- main.py
+++ main.py
@@ -4,4 +4,15 @@ def multiply(num1, num2, num3):
 
 
 def add(num1, num2):
-	return num1 + num2
+    """Adds two numbers together
+
+    Args:
+        num1 ([T]): First number
+        num2 ([T]): Second number
+
+    Returns:
+        [T]: Result of num1 * num2
+    """
+    return num1 + num2
+
+
